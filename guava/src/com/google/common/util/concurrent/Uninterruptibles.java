046ad304e7e35573619a26d01c512825caf5124f
skrauza <slawek.krauza@gmail.com>
2020-04-01 19:14:15 +0200

    #3263 Default timeout fix

--- guava/src/com/google/common/util/concurrent/Uninterruptibles.java
+++ guava/src/com/google/common/util/concurrent/Uninterruptibles.java
@@ -90,6 +90,7 @@ public final class Uninterruptibles {
   public static boolean awaitUninterruptibly(CountDownLatch latch, long timeout, TimeUnit unit) {
     boolean interrupted = false;
     try {
+      if (timeout<0) timeout = 200;
       long remainingNanos = unit.toNanos(timeout);
       long end = System.nanoTime() + remainingNanos;
 
